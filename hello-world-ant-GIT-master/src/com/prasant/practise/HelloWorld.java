6032a4fc4a3938558dcca12db4b81c4e794d62c8
vijaykumar <vijay.kumar@belfricsbt.com>
2018-11-19 12:18:15 +0530

    new

---
+++ hello-world-ant-GIT-master/src/com/prasant/practise/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.prasant.practise;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
