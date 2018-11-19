b66902aecea2af4ec59705e76bc225091b965763
vijaykumar064k <37058055+vijaykumar064k@users.noreply.github.com>
2018-11-19 12:22:17 +0530

    Delete HelloWorld.java

--- hello-world-ant-GIT-master/src/com/prasant/practise/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.prasant.practise;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
-
-}
